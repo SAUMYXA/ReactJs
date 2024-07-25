027e65a968027b2786122fa6ade4217552d8fac6
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2024-07-25 17:42:43 +0530

    understanding props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App(){
+    let expenseDate=new Date(2024,7,25)
+    let expenseTitle='SchoolFee'
+    let  expenseAmt=300;
     return (
         <div>
             <h2>Lets get started!</h2>
-            <ExpenseItem></ExpenseItem>
+            <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmt}></ExpenseItem>
         </div>
         
     )
