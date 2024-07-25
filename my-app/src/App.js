63950fc497a41a91c1b0faf348a340022b7a47d7
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2024-07-25 18:10:19 +0530

    passing data inn expense manager

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,29 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App(){
-    let expenseDate=new Date(2024,7,25)
-    let expenseTitle='SchoolFee'
-    let  expenseAmt=300;
+
+    let expenses=[
+      {  id:'e1',
+        title:'School fees',
+        amount:500,
+        date:new Date(2024,5,5)
+        },
+        {  id:'e2',
+        title:'Electricity Bill',
+        amount:300,
+        date:new Date(2024,5,5)
+        },
+        {  id:'e3',
+        title:'Travel Exp',
+        amount:100,
+        date:new Date(2024,5,5)
+        },
+    ]
     return (
         <div>
             <h2>Lets get started!</h2>
-            <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmt}></ExpenseItem>
+            <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}></ExpenseItem>
+            <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}></ExpenseItem>
+            <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}></ExpenseItem>
         </div>
         
     )
