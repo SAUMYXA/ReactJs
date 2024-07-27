6c487a707ffabd70784ce6ef5a4408bb0f43ac34
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2024-07-27 16:49:58 +0530

    States and two way binding in react

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 
@@ -6,13 +6,14 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    
+    // const [newTitle,setNewTitle]=useState("hi")
     return (
         <Card className="expense-item">
             <ExpenseDate date={new Date(props.date)} />
             <div className="expense-item__description">
                 <h2>{ props.title }</h2>
                 <div className="expense-item__price">${ props.amount }</div>
+                
             </div>
         </Card>
     );
