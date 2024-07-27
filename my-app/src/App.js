e3a934108252ef22d2e72ff1de07cf7348245239
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2024-07-27 13:04:47 +0530

    nesting components in reactjs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,20 @@
-import Expenses from "./components/Expenses";
-function App(){
-
-    let expenses=[
-      {  id:'e1',
-        title:'School fees',
-        amount:500,
-        date:new Date(2024,5,5)
-        },
-        {  id:'e2',
-        title:'Electricity Bill',
-        amount:300,
-        date:new Date(2024,5,5)
-        },
-        {  id:'e3',
-        title:'Travel Exp',
-        amount:100,
-        date:new Date(2024,5,5)
-        },
-    ]
-    return (
-        <div>
-            <h2>Lets get started!</h2>
-             <Expenses item={expenses}/>
-        </div>
-        
-    )
+import Expenses from "./components/Expenses/Expenses";
+function App() {
+  let expenses = [
+    { id: "e1", title: "School fees", amount: 500, date: new Date(2024, 5, 5) },
+    {
+      id: "e2",
+      title: "Electricity Bill",
+      amount: 300,
+      date: new Date(2024, 5, 5),
+    },
+    { id: "e3", title: "Travel Exp", amount: 100, date: new Date(2024, 5, 5) },
+  ];
+  return (
+    <div>
+      <h2>Lets get started!</h2>
+      <Expenses item={expenses} />
+    </div>
+  );
 }
 export default App;
