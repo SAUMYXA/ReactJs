0cc38ac0fe30c4b07a8d2eb3148ca3327fd29864
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2024-07-27 12:30:15 +0530

    organizing the files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 function App(){
 
     let expenses=[
@@ -21,9 +21,7 @@ function App(){
     return (
         <div>
             <h2>Lets get started!</h2>
-            <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}></ExpenseItem>
-            <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}></ExpenseItem>
-            <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}></ExpenseItem>
+             <Expenses item={expenses}/>
         </div>
         
     )
